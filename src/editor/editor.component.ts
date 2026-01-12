0982175dbe4a34f162eb72793a6a7427e390e199
Rafael <rafaeltsd@hotmail.com>
2026-01-12 12:59:41 -0300

    Add konva

--- src/editor/editor.component.ts
+++ src/editor/editor.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+import Konva from 'konva';
 
 @Component({
   selector: 'app-editor',
@@ -7,5 +8,26 @@ import { Component } from '@angular/core';
   templateUrl: './editor.component.html',
   styleUrl: './editor.component.scss'
 })
-export class EditorComponent {
+export class EditorComponent implements AfterViewInit {
+  @ViewChild('konvaContainer', { static: false }) konvaContainer!: ElementRef;
+  private stage!: Konva.Stage;
+  private layer!: Konva.Layer;
+
+  ngAfterViewInit(): void {
+    this.initKonva();
+  }
+
+  private initKonva(): void {
+    const container = this.konvaContainer.nativeElement;
+    const size = Math.min(container.offsetWidth, container.offsetHeight);
+
+    this.stage = new Konva.Stage({
+      container: container,
+      width: size,
+      height: size,
+    });
+
+    this.layer = new Konva.Layer();
+    this.stage.add(this.layer);
+  }
 }
